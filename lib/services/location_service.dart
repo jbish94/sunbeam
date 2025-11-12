6bae7b9c46c4f35f08b6904bcfc3e16dd5275eff
jbish94 <jordanbishop97@gmail.com>
2025-11-12 10:57:20 -0700

    Update location_service.dart

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,11 +1,3 @@
 export 'location_service_stub.dart'
     if (dart.library.html) 'location_service_web.dart'
     if (dart.library.io) 'location_service_mobile.dart';
-
-import 'services/location_service.dart';
-
-final location = LocationService();
-
-await location.init();
-final coords = await location.getLocation();
-print(coords);
